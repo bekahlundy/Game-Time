d65eeb8cabee1e2870f746bb39dd0093677f57c3
Kenia Farias <11431kenia@gmail.com>
2016-11-10 14:57:29 -0700

    testing

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
 require('./carleft-test.js');
 require('./carright-test.js');
 require('./frog-test.js');
+// require('./play-test.js');
---
+++ test/play-test.js
@@ -0,0 +1,13 @@
+// const chai = require('chai');
+// const assert = chai.assert;
+//
+// const Audio = require('../lib/audio.js');
+//
+// describe('Play', function(){
+//   context('with default attributes.', function(){
+//     var play = new Play({});
+//
+//     it('shoul have method playIntro', function(){
+//       var play = new Play();
+//       assert.isFunction();
+//     })
